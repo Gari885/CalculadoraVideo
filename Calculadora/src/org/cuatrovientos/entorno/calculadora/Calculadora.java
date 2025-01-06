3ae6a16c885db338fbedb7b8b022661f6d8a2ece
Gari <garinovoselskyyjaka@gmail.com>
2025-01-06 16:51:02 +0100

    Creaciones de las variables y pedida de los dos numeros

--- Calculadora/src/org/cuatrovientos/entorno/calculadora/Calculadora.java
+++ Calculadora/src/org/cuatrovientos/entorno/calculadora/Calculadora.java
@@ -1,10 +1,22 @@
 package org.cuatrovientos.entorno.calculadora;
+import java.util.Scanner;
 
 public class Calculadora {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new  Scanner(System.in);
+		Integer numeroUno;
+		String conversor;
+		Integer numeroDos;
+		Integer opcion;
+		System.out.println("//CALCULADORA\\");
+		System.out.println("Escribe 2 numeros");
+		System.out.print("Numero 1: ");
+		conversor = scanner.nextLine();
+		numeroUno = Integer.parseInt(conversor);
+		System.out.print("Numero 2: ");
+		conversor = scanner.nextLine();
+		numeroDos = Integer.parseInt(conversor);
 	}
 
 }
