8dc9deb45620bc1b892eff68386747c43969388e
Gari <garinovoselskyyjaka@gmail.com>
2025-01-06 17:05:05 +0100

    Programa terminado y simplificado

--- Calculadora/src/org/cuatrovientos/entorno/calculadora/Calculadora.java
+++ Calculadora/src/org/cuatrovientos/entorno/calculadora/Calculadora.java
@@ -6,44 +6,61 @@ public class Calculadora {
 	public static void main(String[] args) {
 		Scanner scanner = new  Scanner(System.in);
 		Integer numeroUno;
-		String conversor;
 		Integer numeroDos;
 		Integer opcion;
-		Integer respuesta;
 		System.out.println("//CALCULADORA\\");
 		System.out.println("Escribe 2 numeros");
 		System.out.print("Numero 1: ");
-		conversor = scanner.nextLine();
-		numeroUno = Integer.parseInt(conversor);
+		numeroUno = conversion(scanner);
 		System.out.print("Numero 2: ");
-		conversor = scanner.nextLine();
-		numeroDos = Integer.parseInt(conversor);
+		numeroDos = conversion(scanner);
 		do {
 			System.out.println("Elige la operacion que quieres realizar");
 			System.out.println("1. Suma");
 			System.out.println("2. Resta");
 			System.out.println("3. Multiplicacion");
 			System.out.println("4. Salir");
-			conversor = scanner.nextLine();
-			opcion = Integer.parseInt(conversor);
+			opcion = conversion(scanner);
 			
 			switch (opcion) {
 			case 1:
-				respuesta = numeroUno + numeroDos;
-				System.out.println ("La suma de los dos numeros es "+ respuesta);
+				suma(numeroUno, numeroDos);
 				break;
 			case 2:
-				respuesta = numeroUno - numeroDos;
-				System.out.println ("La resta de los dos numeros es "+respuesta);
+				resta(numeroUno, numeroDos);
 				break;
 			case 3:
-				respuesta = numeroUno * numeroDos;
-				System.out.println ("La multiplicacion de los dos numeros es "+respuesta);
+				multiplicacion(numeroUno, numeroDos);
 				break;
 			}
-			
 		} while (opcion != 4);
 		System.out.println("Saliendo......");
+	}
+
+	private static void multiplicacion(Integer numeroUno, Integer numeroDos) {
+		Integer respuesta;
+		respuesta = numeroUno * numeroDos;
+		System.out.println ("La multiplicacion de los dos numeros es "+respuesta);
+	}
+
+	private static void resta(Integer numeroUno, Integer numeroDos) {
+		Integer respuesta;
+		respuesta = numeroUno - numeroDos;
+		System.out.println ("La resta de los dos numeros es "+respuesta);
+	}
+
+	private static void suma(Integer numeroUno, Integer numeroDos) {
+		Integer respuesta;
+		respuesta = numeroUno + numeroDos;
+		System.out.println ("La suma de los dos numeros es "+ respuesta);
+	}
+
+	private static Integer conversion(Scanner scanner) {
+		Integer numeroUno;
+		String conversor;
+		conversor = scanner.nextLine();
+		numeroUno = Integer.parseInt(conversor);
+		return numeroUno;
 	}
 
 }
